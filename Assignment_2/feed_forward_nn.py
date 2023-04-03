fixed CE, backprop and evaluation

Assignment_2/feed_forward_nn.py
@@ -3,52 +3,89 @@ import pandas as pd
 import load_mnist as lm
 import matplotlib.pyplot as plt
 
+
+
+
 class NeuralNetwork:
     # Create a neural network with #hidden layers and #neurons in each layer
     def __init__(self, features, learningRate, *args):
         self.features = features
         self.args = args
         self.learningRate = learningRate
-        self.weights = None
-        self.bias = None
-        self.dW = None
-        self.dB = None
+        self.weights1 = None
+        self.bias1 = None
+        self.dL = None
+        self.bA1 = None
+        self.dZ1 = None
+        self.dW1 = None
+        self.dB1 = None
         self.training_history = []
        
     # Create arrays with random parameters 
     def initiliaze_parameters(self):
-        self.weights = np.zeros((self.features,1))
-        self.bias = 0
+       # np.random.seed(42)
+        self.weights1 = np.random.rand(10, 784)
+        self.bias1 = np.random.rand(10, 1)
 
+    def softmax(self, Z):
+        A = np.exp(Z) / sum(np.exp(Z))
+        return A
+
+    def sigmoid(self, Z):
+        A = np.exp(Z)/(1 + np.exp(Z))
+        return A
     
+    def relu(self, Z):
+        A = np.maximum(Z,0)
+        return A
+
+
     # Run model forward with the input x 
     def model_forward(self, X): 
-        y_pred = np.dot(X, self.weights) + self.bias
-        return y_pred
+        Z1 = np.dot(self.weights1, X) + self.bias1  # 10, m
+        A1 = self.softmax(Z1)                   # 10, m
+        #print('model forward')
+        #print(Z1.shape)
+        #print(A1.shape)
+        return Z1, A1
 
 
     def compute_cost(self, Y_pred, Y):
         return (Y_pred - Y)
 
+    def cross_entropy(self, Y_pred, Y):
+        cost = -np.sum(Y.dot(np.log(Y_pred.T)))
+        #print(cost)
+        #print('cross entropy')
+        #print(cost.shape)
+        return cost
 
+   # [1, 784] x [784, 10] + [1,10] = [1, 10]  
     #Calculate the gradients 
     def model_backward(self, X, Y):
-        samples, features = X.shape
-        Y_pred = self.model_forward(X)
-        cost = self.compute_cost(Y_pred, Y)
-        
-        self.dW = (2/samples) * np.dot(X.T, cost) 
-        self.dB = (2/samples) * np.sum(cost)
-        self.training_history.append(np.mean(np.abs(cost)))
+        samples, features = 53, 748 # X.shape
+        Z1, A1 = self.model_forward(X) 
+        L = 1/samples * self.cross_entropy(A1,Y)
+    
+        self.dZ1 = 1/samples * (-Y + A1)          # 10 m
+        #print('model backward')
+        #print(self.dZ1.shape)
+        self.dW1 = (1/samples) * np.dot(self.dZ1, X.T)  # 10, 784  
+        #print(self.dW1.shape)
+        self.dB1 = (1/samples) * np.reshape(np.sum(self.dZ1,1),(10,1)) # 10, 1
+        #print(self.dB1.shape)
+        self.training_history.append(np.mean(np.abs(self.dZ1)))
 
-    #Update the weight and bias with the pre-calculated gradients
+    #Update the weight and bias with the pre-calciulated gradients
     def update_parameters(self):
-        self.weights -= self.learningRate * self.dW
-        self.bias -= self.learningRate * self.dB
+        self.weights1 -= self.learningRate * self.dW1
+        self.bias1 -= self.learningRate * self.dB1
 
     #Forward run
     def predict(self, X):
-        return self.model_forward(X)
+        Z1, A1 = self.model_forward(X)
+        return A1
+    
 
     #Train the network
     def train_linear_model(self, X, Y, iterations):
@@ -58,8 +95,8 @@ class NeuralNetwork:
 
 
     def print_parameters(self):
-        print(self.weights)
-        print(self.bias)
+        print(self.weights1)
+        print(self.bias1)
     
     def history(self):
        return self.training_history
@@ -67,3 +104,57 @@ class NeuralNetwork:
 
 X_train, Y_train, X_test, Y_test = lm.load_mnist()
 
+
+assert X_train.shape[0] == Y_train.shape[0], "Mismatch in the number of samples between data and labels."
+
+def shuffle_data_and_labels(data, labels):
+    # Get the number of samples
+    n_samples = data.shape[0]
+
+    # Create an array of indices representing the samples
+    indices = np.arange(n_samples)
+
+    # Shuffle the indices
+    np.random.shuffle(indices)
+
+    # Rearrange the data and labels using the shuffled indices
+    shuffled_data = data[indices]
+    shuffled_labels = labels[indices]
+
+    return shuffled_data, shuffled_labels
+
+# Shuffle the data and labels
+shuffled_data, shuffled_labels = shuffle_data_and_labels(X_train, Y_train)
+shuffled__testdata, shuffled_testlabels = shuffle_data_and_labels(X_test, Y_test)
+
+
+nn = NeuralNetwork(784, 1e-2, 1)
+nn.initiliaze_parameters()
+nn.train_linear_model(shuffled_data.T, shuffled_labels.T, 700)
+plt.plot(nn.history())
+plt.show()
+
+# Generate sample 2D NumPy arrays of size (10, 10000) with random values
+arr1 = shuffled_testlabels.T
+arr2 = nn.predict(shuffled__testdata.T)
+
+def compare_max_indices(arr1, arr2):
+    # Make sure both arrays have the same shape
+    assert arr1.shape == arr2.shape, "Arrays must have the same shape."
+
+    # Get the indices of the maximum values along axis 0 (rows)
+    max_indices_arr1 = np.argmax(arr1, axis=0)
+    max_indices_arr2 = np.argmax(arr2, axis=0)
+
+    # Compare the indices and count the matches
+    matches = np.sum(max_indices_arr1 == max_indices_arr2)
+
+    # Calculate the percentage of correct matches
+    percentage = (matches / arr1.shape[1]) * 100
+
+    return percentage
+
+# Calculate the percentage of correct matches
+correct_percentage = compare_max_indices(arr1, arr2)
+
+print("Percentage of correct matches:", correct_percentage)

Assignment_2/load_mnist.py
@@ -11,7 +11,7 @@ def load_mnist():
     test_labels = []    
     
     for label in range(NUM_LABELS):
-        for image_path in glob.glob("MNIST/Test/" + str(label) + "/*.png"):
+        for image_path in glob.glob("/Users/falk/Documents/python_projects/Deep_learning_for_image_analysis/Assignment_2/MNIST/Test/" + str(label) + "/*.png"):
             image = imageio.imread(image_path)
             test_images.append(image)
             letter = [0 for _ in range(0,NUM_LABELS)]    
@@ -23,7 +23,7 @@ def load_mnist():
     train_labels = []    
     
     for label in range(NUM_LABELS):
-        for image_path in glob.glob("MNIST/Train/" + str(label) + "/*.png"):
+        for image_path in glob.glob("/Users/falk/Documents/python_projects/Deep_learning_for_image_analysis/Assignment_2/MNIST/Train/" + str(label) + "/*.png"):
             image = imageio.imread(image_path)
             train_images.append(image)
             letter = [0 for _ in range(0,NUM_LABELS)]    
@@ -34,5 +34,4 @@ def load_mnist():
     Y_train= np.array(train_labels)
     X_test= np.array(test_images).reshape(-1,784)/255.0
     Y_test= np.array(test_labels)
-    
     return X_train, Y_train, X_test, Y_test