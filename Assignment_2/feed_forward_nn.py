2405447f0020a0bec0fa0dd7ec455ae64debc1b1
Linus Falk <linus.falk@regal.se>
2023-04-06 22:41:48 +0200

    one forward and backward working.

--- Assignment_2/feed_forward_nn.py
+++ Assignment_2/feed_forward_nn.py
@@ -24,7 +24,7 @@ class NeuralNetwork:
         
         self.layers = []
         self.layersIO = []
-        self.layerBackward = []
+        self.layerGradients = []
 
         self.training_history = []
         self.training_history_test = []
@@ -126,31 +126,39 @@ class NeuralNetwork:
         bias = np.random.rand(nodes[1], 1)
         self.layers.append([weights, bias, activation]) #[weights, bias, activation function]
         self.layersIO.append([None, None, None]) # [input, Z, A]
-        self.layerBackward.append([None, None, None, None]) #[dA, dZ, dW, dB]
+        self.layerGradients.append([None, None, None, None]) #[dZ, dW, dB, dA]
 
     def activation_backward(self, layer, Y):
         if self.layers[layer][2] == 'relu':
-            dA = self.relu_backward(self.layersIO[layer][1])
-            self.layerBackward[layer][0]
+            dZ = self.relu_backward(self.layersIO[layer][1])
+            self.layerGradients[layer][0] = dZ
 
         elif self.layers[layer][2] == 'sigmoid':
            pass
 
+        elif self.layers[layer][2] == 'softmax':
+            dA = - Y + self.layersIO[layer][2]
+            self.layerGradients[layer][3] = dA
+            
+
+
         
 
-    def linear_backward(self, layer, Y):
-        '''
-        self.dW2 = (1/samples) * np.dot(self.dZ2, A1_train.T) 
-        self.dB2 = (1/samples) * np.reshape(np.sum(self.dZ2,1),(10,1)) 
 
-        self.dZ1 = np.dot(self.weights2.T,self.dZ2) * self.relu_grad(Z1_train)          
-        self.dW1 = (1/samples) * np.dot(self.dZ1, X.T)  
-        self.dB1 = (1/samples) * np.reshape(np.sum(self.dZ1,1),(100,1))
-        '''
-        dZ = np.dot(self.layers[layer+1][0].T, self.layerBackward[layer][0]) * self.layerBackward[layer][0]      
-        dW = (1/samples) * np.dot(self.dZ1, X.T)  
-        dB = (1/samples) * np.reshape(np.sum(self.dZ1,1),(100,1))
+    def linear_backward(self, layer):
+        if self.layers[layer][2] == 'softmax':
+            dZ = self.layerGradients[layer][3]
+            dW = (1/600) * np.dot(dZ, self.layersIO[layer-1][1].T)
+            dB = (1/600) * np.reshape(np.sum(dZ,1), (10,1))
 
+            self.layerGradients[layer] = [dZ, dW, dB]
+
+        else:
+            dZ = np.dot(self.layers[layer+1][0].T, self.layerGradients[layer+1][0]) * self.layerGradients[layer][0]
+            dW = (1/600) * np.dot(self.layerGradients[layer][0], self.layersIO[layer][0].T)
+            dB = (1/600) * np.reshape(np.sum(self.layerGradients[layer][0],1), (100,1))
+       
+            self.layerGradients[layer] = [dZ, dW, dB]
 
     def softmax_backward(self,layer, Y):
         pass
@@ -165,8 +173,10 @@ class NeuralNetwork:
 
 
     def update_parameters_new(self):
-        pass
-
+        for i in range(len(self.layers)):
+            print(i)
+            self.layers[i][0] -= self.learningRate * self.layerGradients[i][1]
+            self.layers[i][1] -= self.learningRate * self.layerGradients[i][2]
 
     #Update the weight and bias with the pre-calciulated gradients
     def update_parameters(self):
@@ -223,7 +233,7 @@ class NeuralNetwork:
     def weights_as_image(self):
         fig, ax = plt.subplots(2,5, dpi=200)   
         for x in range(5):
-            im1 = self.weights1[x,:].reshape((28,28))
+            im1 = self.layers[0][0][x,:].reshape((28,28))
             im2 = self.weights1[5+x,:].reshape((28,28))
             ax[0,x].imshow(im1)
             ax[0,x].set_xticks([]) 
@@ -300,18 +310,24 @@ mini_batches_test = create_mini_batches(shuffled_testdata, shuffled_testlabels, 
 nn = NeuralNetwork(784, 1e-2, 1)
 nn.initiliaze_parameters()
 
-nn.create_layer([784, 40], 'relu')
-nn.create_layer([40, 40], 'relu')
-nn.create_layer([40, 10], 'softmax')
+nn.create_layer([784, 100], 'relu')
+nn.create_layer([100, 10], 'softmax')
+
 nn.print_layer()
+
 
 nn.linar_forward(0, mini_batches[0][0].T)
 nn.activation_forward(0)
 nn.linar_forward(1, nn.layersIO[0][2])
 nn.activation_forward(1)
-nn.linar_forward(2, nn.layersIO[1][2])
-nn.activation_forward(2)
+nn.activation_backward(1, mini_batches[0][1].T)
+nn.linear_backward(1)
+nn.activation_backward(0, mini_batches[0][1].T)
+nn.linear_backward(0)
+nn.update_parameters_new()
+nn.print_layer()
 
+#TODO one backward pass works, index out of range in the second???
 
 
 epochs = 1
