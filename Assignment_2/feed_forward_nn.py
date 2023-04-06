63a8de9f4e1e47ff4decd236eff50bea48679c07
Linus Falk <linus.falk@regal.se>
2023-04-06 15:00:19 +0200

    feed forward with new layer design working

--- Assignment_2/feed_forward_nn.py
+++ Assignment_2/feed_forward_nn.py
@@ -10,6 +10,7 @@ class NeuralNetwork:
         self.features = features
         self.args = args
         self.learningRate = learningRate
+        
         self.weights1 = None
         self.bias1 = None
         self.weights2 = None
@@ -20,7 +21,9 @@ class NeuralNetwork:
         self.dZ2 = None
         self.dW2 = None
         self.dB2 = None
+        
         self.layers = []
+        self.layerOutput = []
         self.training_history = []
         self.training_history_test = []
        
@@ -51,6 +54,28 @@ class NeuralNetwork:
 
     def relu_grad(self, Z):
         return Z > 0
+
+    def linar_forward(self, layer , X):
+        print(self.layers[layer][0].shape)
+        print(X.shape)
+
+        Z = np.dot(self.layers[layer][0], X) + self.layers[layer][1]   
+        self.layerOutput[layer][0] = Z
+    
+    def activation_forward(self, layer):
+        if self.layers[layer][2] == 'relu':
+            A = self.relu(self.layerOutput[layer][0])
+            self.layerOutput[layer][1] = A
+
+        elif self.layers[layer][2] == 'sigmoid':
+            A = self.sigmoid(self.layerOutput[layer][0])
+            self.layerOutput[layer][1] = A
+        
+        elif self.layers[layer][2] == 'softmax':
+            A = self.softmax(self.layerOutput[layer][0])
+            self.layerOutput[layer][1] = A
+         
+        
 
     # Run model forward with the input x 
     def model_forward(self, X): 
@@ -93,24 +118,29 @@ class NeuralNetwork:
         self.dW1 = (1/samples) * np.dot(self.dZ1, X.T)  
         self.dB1 = (1/samples) * np.reshape(np.sum(self.dZ1,1),(100,1))
 
-    def create_layers(self, nodes, activation):
+
+    def create_layer(self, nodes, activation):
         weights = np.random.rand(nodes[1], nodes[0]) * np.sqrt(1 / 784) 
         bias = np.random.rand(nodes[1], 1)
         self.layers.append([weights, bias, activation])
-        self.variables.append([None, None, None])
+        self.layerOutput.append([None, None])
+        # Z1, A1
+
 
     def linear_backward(self):
         pass
 
+
     def relu_backward(self):
         pass
+
 
     def sigmoid_backward(self):
         pass
 
+
     def update_parameters_new(self):
         pass
-
 
     #Update the weight and bias with the pre-calciulated gradients
     def update_parameters(self):
@@ -136,6 +166,8 @@ class NeuralNetwork:
 
        
      #Predict and calculate percentage correct
+    
+
     def predict(self, X, Y):
         Z1, A1, Z2, A2 = self.model_forward(X)
         percent = self.compare(A2, Y)
@@ -153,9 +185,15 @@ class NeuralNetwork:
         print(self.weights2.shape)
         print(self.bias2.shape)
     
+    def print_layer(self):
+        for i in range(len(self.layers)):
+            print(self.layers[i][0].shape)
+
+
     def history(self):
        return self.training_history, self.training_history_test
     
+
     def weights_as_image(self):
         fig, ax = plt.subplots(2,5, dpi=200)   
         for x in range(5):
@@ -236,14 +274,27 @@ mini_batches_test = create_mini_batches(shuffled_testdata, shuffled_testlabels, 
 nn = NeuralNetwork(784, 1e-2, 1)
 nn.initiliaze_parameters()
 
+nn.create_layer([784, 40], 'relu')
+nn.create_layer([40, 40], 'relu')
+nn.create_layer([40, 10], 'softmax')
+nn.print_layer()
 
-epochs = 100
+nn.linar_forward(0, mini_batches[0][0].T)
+nn.activation_forward(0)
+nn.linar_forward(1, nn.layerOutput[0][1])
+nn.activation_forward(1)
+nn.linar_forward(2, nn.layerOutput[1][1])
+nn.activation_forward(2)
+
+
+
+epochs = 1
 history = []
 history_test = []
 
 # Train the network for number of epochs
 for y in range(epochs):
-    print(str(y) + ' out of ' + str(epochs) + ' epochs')
+    print(str(y+1) + ' out of ' + str(epochs) + ' epochs')
     for x in range(len(mini_batches)):
         nn.train_linear_model(mini_batches[x][0].T, mini_batches[x][1].T, mini_batches_test[x][0].T, mini_batches_test[x][1].T,   1)
         #hist, hist_test = nn.history()
